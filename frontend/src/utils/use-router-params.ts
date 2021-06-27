3e3da1e37702be2527b8a40fbbdf004110f55156
石丸 元気 <genki140@gmail.com>
2021-06-27 09:31:00 +0000

    トークンに何でも情報を持たせるのをやめ、graphqlのcurrentUserで必要な情報を取得する

--- frontend/src/utils/use-router-params.ts
+++ frontend/src/utils/use-router-params.ts
@@ -1,15 +1,15 @@
-import { useStoreState } from '@/ducks/store';
+import { useGetCurrentUserQuery } from '@/types/graphql';
 import { useRouter } from 'next/router';
 
 export const useRouterParams = () => {
   const router = useRouter();
-  const user = useStoreState((x) => x.loginUser);
-
   const organizationName = (router.query.organizationName ?? '').toString();
   const areaName = (router.query.areaName ?? '').toString();
 
+  const currentUser = useGetCurrentUserQuery();
+
   const getOrganizationId = () => {
-    const value = user?.userOrganizations.find(
+    const value = currentUser.data?.currentUser.userOrganizations.find(
       (x) => x.organization.id.toString() === organizationName || x.organization.name === organizationName,
     )?.organization.id;
     return value != null ? Number(value) : 0;
@@ -18,11 +18,12 @@ export const useRouterParams = () => {
   return {
     organizationName,
     areaName,
+    hasOrganization: organizationName !== '',
     hasOrganizationAndArea: organizationName !== '' && areaName !== '',
     // 組織名文字列からIDを取得
     getOrganizationId,
     getAreaId: () => {
-      const value = user?.userOrganizations
+      const value = currentUser.data?.currentUser.userOrganizations
         .find((x) => x.organization.id === getOrganizationId())
         ?.organization.areas.find((x) => x.id.toString() === areaName || x.name === areaName)?.id;
       return value != null ? Number(value) : 0;
